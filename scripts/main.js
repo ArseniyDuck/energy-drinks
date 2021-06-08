53e2a11b5ea08b628586ce07e6c6526a1ad1773d
ArseniyDuck <67900946+ArseniyDuck@users.noreply.github.com>
2021-06-08 18:57:24 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+// Hearts
+const heartsOff = Array.from(document.querySelectorAll("img[alt='heart off']"));
+const heartsOn = Array.from(document.querySelectorAll("img[alt='heart on']"));
+
+
+document.addEventListener("click", () => {
+    if (heartsOff.indexOf(event.target) != -1) {
+        event.target.previousElementSibling.classList.toggle("active");
+    } else if (heartsOn.indexOf(event.target) != -1) {
+        event.target.classList.toggle("active");
+    }
+});
+
+// AOS scroll animation
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 100, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease-out', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
